019527d3ffda2393ddad7376ffec26d0dc89b1b5
Giorgio Comai <g@giorgiocomai.eu>
2023-09-04 10:05:52 +0200

    cc_get_fb_ads_campaigns now pages results (but does not cache them)

--- R/cc_get_fb_ads_campaigns.R
+++ R/cc_get_fb_ads_campaigns.R
@@ -1,8 +1,10 @@
 #' Get all campaigns for the current ad account
 #'
-#' For details on valid fields, see: https://developers.facebook.com/docs/marketing-api/reference/ad-campaign-group/
+#' For details on fields, see: https://developers.facebook.com/docs/marketing-api/reference/ad-campaign-group/
 #'
-#' Draft: not yet caching or paging results
+#' It currently returns only fields that are always present for all campaigns: "id,name,created_time,updated_time,start_time,stop_time,objective,status"
+#'
+#' Draft: paging results, but not yet caching
 #'
 #' @return
 #' @export
@@ -13,7 +15,9 @@
 #' }
 cc_get_fb_ads_campaigns <- function(api_version = "v17.0",
                                     ad_account_id = NULL,
-                                    token = NULL) {
+                                    max_pages = NULL,
+                                    token = NULL,
+                                    cache = FALSE) {
   if (is.null(token)) {
     fb_user_token <- cc_get_settings(fb_user_token = token) |>
       purrr::pluck("fb_user_token")
@@ -33,6 +37,12 @@ cc_get_fb_ads_campaigns <- function(api_version = "v17.0",
     api_version
   )
 
+  out <- vector("list", max_pages %||% 1000)
+  campaigns_l <- vector("list", max_pages %||% 1000)
+
+  i <- 1L
+  cli::cli_progress_bar(name = "Retrieving Facebook ad campaigns:")
+
   api_request <- httr2::request(base_url = base_url) |>
     httr2::req_url_path_append(stringr::str_c("act_", fb_ad_account_id)) |>
     httr2::req_url_path_append("campaigns") |>
@@ -41,23 +51,64 @@ cc_get_fb_ads_campaigns <- function(api_version = "v17.0",
       access_token = fb_user_token
     )
 
-  req <- httr2::req_perform(req = api_request)
+  repeat({
+    cli::cli_progress_update(inc = 25)
 
-  response_l <- httr2::resp_body_json(req) |>
-    purrr::pluck("data")
+    out[[i]] <- httr2::req_perform(api_request) |>
+      httr2::resp_body_json()
 
-  campaigns_df <- purrr::map(
-    .x = response_l,
-    .f = function(x) {
-      x |>
-        tibble::as_tibble()
+    if (!is.null(max_pages) && i == max_pages) {
+      break
     }
-  ) |>
-    purrr::list_rbind() |>
-    dplyr::rename(
-      campaign_id = id,
-      campaign_name = name
-    )
 
-  campaigns_df
+    req <- httr2::req_perform(req = api_request)
+
+    response_l <- httr2::resp_body_json(req) |>
+      purrr::pluck("data")
+
+    current_campaigns_df <- purrr::map(
+      .x = response_l,
+      .f = function(x) {
+        x |>
+          tibble::as_tibble()
+      }
+    ) |>
+      purrr::list_rbind() |>
+      dplyr::rename(
+        campaign_id = id,
+        campaign_name = name
+      ) |>
+      dplyr::mutate(timestamp_retrieved = strftime(as.POSIXlt(Sys.time(), "UTC"), "%Y-%m-%dT%H:%M:%S%z"))
+
+    campaigns_l[[i]] <- current_campaigns_df
+
+    if (nrow(current_campaigns_df) == 0) {
+      break
+    } else {
+      if (cache == TRUE) {
+        DBI::dbAppendTable(
+          conn = db,
+          name = current_table,
+          value = current_campaigns_df
+        )
+      }
+    }
+
+    if (purrr::pluck_exists(out[[i]], "paging", "next") == TRUE) {
+      api_request <- purrr::pluck(out[[i]], "paging", "next") |>
+        httr2::request()
+    } else {
+      break
+    }
+
+    i <- i + 1L
+    if (i > length(out)) {
+      length(out) <- length(out) * 2L
+    }
+  })
+
+  cli::cli_process_done()
+
+  campaigns_l |>
+    purrr::list_rbind()
 }
