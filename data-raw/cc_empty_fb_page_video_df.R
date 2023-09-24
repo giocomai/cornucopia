0f3822ac8f2fff5a3867e192c36db291510c28ac
Giorgio Comai <g@giorgiocomai.eu>
2023-09-24 23:39:54 +0200

    introduce get fb page video id (including reels)

---
+++ data-raw/cc_empty_fb_page_video_df.R
@@ -0,0 +1,34 @@
+## code to prepare `cc_empty_fb_page_video_df` dataset goes here
+
+
+cc_empty_fb_page_video_df <- tibble::tibble(
+  created_time = character(),
+  id = character(),
+  
+  title = character(),
+  description = character(),
+  
+  post_id = character(),
+  post_views = character(),
+
+  views = character(),  
+  
+  permalink_url = character(),
+  
+  embed_html = character(),
+  embeddable = character(),
+  
+  source = character(),
+  
+  icon = character(),
+  is_instagram_eligible = character(),
+  
+  length = character(),
+  live_status = character(),
+
+  universal_video_id = character(),
+
+  updated_time = character()
+)
+
+usethis::use_data(cc_empty_fb_page_video_df, overwrite = TRUE)
